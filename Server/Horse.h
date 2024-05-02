c6e612e17a9d129bef4cdca014eab3e5a5fac625
hypernagox <128601833+hypernagox@users.noreply.github.com>
2024-05-02 23:31:37 +0900

    horse

--- Server/Horse.h
+++ Server/Horse.h
@@ -5,14 +5,15 @@ class Horse
 {
 public:
 	Horse();
+	~Horse();
 	std::pair<POINT,Vec2> UpdateHorsePos(const int VK);
-	static Vec2 GetInitPos() { return g_board.GetBoardPos(0, 0); }
+	//static Vec2 GetInitPos() { return g_board->GetBoardPos(0, 0); }
 public:
 	int m_xPos = 0;
 	int m_yPos = 0;
 	Vec2 m_vPos = {};
 public:
 	class ClientSession* m_pSession = nullptr;
-	static inline Chess g_board;
+	static inline std::shared_ptr<Chess> g_board[400 / SECTOR_SIZE][400 / SECTOR_SIZE];
 };
 
