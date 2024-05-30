797fde0b11f06645f5cafa5f1abf7fd3390ee889
hypernagox <128601833+hypernagox@users.noreply.github.com>
2024-05-30 23:59:57 +0900

    a

--- Server/Chess.h
+++ Server/Chess.h
@@ -10,7 +10,7 @@ constexpr const int VIEW_RANGE = 11;
 //constexpr const int VIEW_RANGE = 0;	// 0으로 하면 시야처리 X와 같음 (함수자체에 constexpr을 사용하였습니다.)
 constexpr const int NPC_RANGE = VIEW_RANGE - 5;	// 플레이어가 NPC를 깨우는 사거리
 constexpr const int POSSIBILITY = 5; // NPC 딜레이 출력메세지 확률 (플레이어가 처음 깨웠다면 그때는 바로 작동하기 때문에 0ms가 나올 수 있습니다.)
-constexpr const bool POSSIBILITY_ON = true;	// false로 바꾸면 출력하지 않음
+constexpr const bool POSSIBILITY_ON = false;	// false로 바꾸면 출력하지 않음
 
 class Chess
 	:public ServerCore::SessionManageable
